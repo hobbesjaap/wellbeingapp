0f5642f4fb172dd57b50b76ce3f4b21f87642c76
Jaap Marsman <jmarsman@me.com>
2022-08-10 17:33:29 +0800

    continued work on UI

--- godot/user_values.gd
+++ godot/user_values.gd
@@ -1,6 +1,15 @@
 extends Node
 
 var user_first_name = "Not defined"
+var user_birth_month
+var user_birth_date
+
+# For the login-streak thing
+
+#var user_login_streak
+#var user_last_login_year
+#var user_last_login_month
+#var user_last_login_day
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
