b52041d6d17a5be4b45addd020bc0523376f430c
Jaap Marsman <jmarsman@me.com>
2022-08-17 20:57:17 +0800

    continued UI work - User Info screen + fonts added

--- godot/node_variables.gd
+++ godot/node_variables.gd
@@ -7,8 +7,8 @@ onready var user_menu = get_node("/root/main_screen/UI/top_container/user_detail
 onready var daily_prompts = get_node("/root/main_screen/UI/top_container/daily_prompts")
 
 
-onready var user_name = get_node("/root/main_screen/UI/top_container/user_details/user_menu/textbox_user_name")
-onready var first_time_label = get_node("/root/main_screen/UI/top_container/user_details/user_menu/label_first_time")
+onready var user_name = get_node("/root/main_screen/UI/top_container/user_details/user_menu/green_panel/textbox_user_name")
+onready var first_time_label = get_node("/root/main_screen/UI/top_container/user_details/user_menu/green_panel/label_first_time")
 onready var welcome_label = get_node("/root/main_screen/UI/top_container/start_menu/welcome_title")
 
 
